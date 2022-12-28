0a3562f0b8d2a711941f0da5df458169bd809e14
giota <giotafel96@gmail.com>
2022-12-28 21:26:38 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-import pandas as pd
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("./finance_liquor_sales.csv")
-df.head()
+df = pd.read_csv("finance_liquor_sales.csv")
+cn = df.groupby(['zip_code','bottles_sold'])
+print(cn.first().to_string())
 
+
+plt.plot([50000, 53000], [0, 1000])
+plt.title("Bottles Sold")
+plt.xlabel("X - Zip Code")
+plt.ylabel("y - Bottles Sold")
+df.plot(kind='scatter',x='x',y='y')
+plt.show()
