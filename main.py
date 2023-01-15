d92383e1974f222c2dddc08b64a92c11b00fde94
giota <giotafel96@gmail.com>
2023-01-15 14:24:08 +0200

    Initial commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 
 df = pd.read_csv("finance_liquor_sales.csv")
 cn = df.groupby(['zip_code','bottles_sold'])
-print(cn.first().to_string())
+#print(cn.first().to_string())
 
 
 plt.plot([50000, 53000], [0, 1000])
